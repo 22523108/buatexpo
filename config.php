33e18714025014baaf5d845edb26962022cc1322
Your Name <your.email@example.com>
2023-12-02 06:14:29 +0700

    cofigphp berubah

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
+$servername = "localhost:4306"; //harus merubah pot di config xampp mysql nya
+$username = "root";
+$dbname = "expo_db";
+$password = "";
 
-$conn = mysqli_connect('localhost','root','','expo_db') or die('connection failed');
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn){
+    die("Gagal". mysqli_connect_error());
+}
 ?>
